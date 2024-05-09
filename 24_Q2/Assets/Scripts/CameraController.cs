26d405e3bcb7546d08ff19759607f734d21d40a7
HanSolme <101610067+konic3@users.noreply.github.com>
2024-05-09 18:02:09 +0900

    카메라 고정

--- 24_Q2/Assets/Scripts/CameraController.cs
+++ 24_Q2/Assets/Scripts/CameraController.cs
@@ -1,12 +1,53 @@
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
-{
-    public Transform target;    // 타겟 위치
-    public Vector3 offset;      // 초기 값 
+{   
+    [SerializeField]
+    Transform playerTransform;
+    [SerializeField]
+    Vector3 cameraPosition;
 
-    void Update()
+    [SerializeField]
+    Vector2 center;
+    [SerializeField]
+    Vector2 mapSize;
+
+    [SerializeField]
+    float cameraMoveSpeed;
+    float height;
+    float width;
+
+    void Start()
     {
-        transform.position = new Vector3(target.position.x, 0, 0)  + offset;
+        playerTransform = GameObject.Find("Player").GetComponent<Transform>();
+
+        height = Camera.main.orthographicSize;
+        width = height * Screen.width / Screen.height;
     }
+
+    void FixedUpdate()
+    {
+        LimitCameraArea();
+    }
+
+    void LimitCameraArea()
+    {
+        transform.position = Vector3.Lerp(transform.position,
+                                          playerTransform.position + cameraPosition,
+                                          Time.deltaTime * cameraMoveSpeed);
+        float lx = mapSize.x - width;
+        float clampX = Mathf.Clamp(transform.position.x, -lx + center.x, lx + center.x);
+
+        float ly = mapSize.y - height;
+        float clampY = Mathf.Clamp(transform.position.y, -ly + center.y, ly + center.y);
+
+        transform.position = new Vector3(clampX, clampY, -10f);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(center, mapSize * 2);
+    }
+
 }
